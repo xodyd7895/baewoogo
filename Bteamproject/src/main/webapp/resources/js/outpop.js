021b7fa401633add2bd6422adcff6ebdb7887f27
KMS <KMS@192.168.219.104>
2022-05-15 17:45:45 +0900

    강민성

--- Bteamproject/src/main/webapp/resources/js/outpop.js
+++ Bteamproject/src/main/webapp/resources/js/outpop.js
@@ -3,9 +3,13 @@ var modal = document.querySelector("#modal");
 var bac = document.querySelector("#bac");
 
 
+
+
 [].forEach.call(buttons, function (button) {
   button.addEventListener("click", function () {
+  document.body.classList.add("stop-scroll");
     modal.classList.toggle("off");
     
   });
 });
+
